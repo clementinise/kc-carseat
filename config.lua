ef63d2fef46927e06b2463807f787a4fc5176c08
clementinise <35346472+clementinise@users.noreply.github.com>
2023-04-17 09:35:29 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,11 +5,12 @@ Config.WaitForEnterInput            = 300       -- Longer wait times allow for m
 Config.WaitForExitInput             = 200       -- Longer wait times allow for more time to double press, in milliseconds. If you don't want to use this feature, set it to 0
 Config.Distance                     = 6.0       -- The distance from the vehicle beyond which the function should not activate
 Config.CheckForMovements            = true      -- Should it detects if the player presses any move key (S, D, A except Z) like in regular GTA when entering cars?
+Config.LeaveEngineStaysRunning      = true      -- Should the engine stay running when a player exits a vehicle, regardless of whether they long/double press or not?
 
 -- Locale
 Config.Language						= 'en'		-- Currently Available: fr, en
 
 -- Misc
-Config.Debug                        = false     -- If you suspect that something isn't working correctly, set 'Config.Debug' to true. This will print debug logs in your console
+Config.Debug                        = true      -- If you suspect that something isn't working correctly, set 'Config.Debug' to true. This will print debug logs in your console
 Config.UpdateChecker                = false     -- Set to false if you don't want to check for resource updates on startup (No need to change this if you're using fivem-checker)
 Config.ChangeLog                    = false     -- Set to false if you don't want to display the changelog when a new version is found (No need to change this if you're using fivem-checker)
